1b0160be1c3674a07a14d279cd5d9ef0ceb0594b
mazyhar <mazhar.hossin1@gmail.com>
2024-10-27 05:09:07 -0500

    updating scanner area

--- ticketingsystem_fe/src/app/components/scanner/scanner.component.ts
+++ ticketingsystem_fe/src/app/components/scanner/scanner.component.ts
@@ -45,7 +45,7 @@ export class ScannerComponent implements OnInit, AfterViewInit, OnDestroy {
         { facingMode: 'environment' },
         {
           fps: 10,
-          qrbox: { width: window.innerWidth * .70, height: window.innerHeight * .85 },
+          qrbox: { width: window.innerWidth * .50, height: window.innerHeight * .50 },
         },
         (decodedText) => this.handleQrCodeSuccess(decodedText),
         (errorMessage) => console.log('QR Code scan error:', errorMessage)
